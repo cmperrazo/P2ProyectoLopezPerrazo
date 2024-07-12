557e925c24c126b75cdd8af9db9f62f054a5c8a6
raysonlopez <rslopez6@espe.edu.ec>
2024-07-11 20:19:44 -0500

    Inicio del Proyecto Grupal Parcial 2

---
+++ src/main/java/com/mycompany/p2proyectogrupallopezrayson/P2ProyectoGrupalLopezRayson.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p2proyectogrupallopezrayson;
+
+/**
+ *
+ * @author Rayson
+ */
+public class P2ProyectoGrupalLopezRayson {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
